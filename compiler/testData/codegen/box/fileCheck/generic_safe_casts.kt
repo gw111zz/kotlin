4490b56738486fbf8973661451641d25bbdd11ea
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-01-30 11:12:02 +0200

    fixup! [K/N][tests] Added some file-check tests on generic safe casts

--- compiler/testData/codegen/box/fileCheck/generic_safe_casts.kt
+++ compiler/testData/codegen/box/fileCheck/generic_safe_casts.kt
@@ -8,8 +8,19 @@ value class Foo(val value: Int)
 // CHECK-LABEL: call %struct.ObjHeader* @"kfun:Foo#toString(){}kotlin.String
 // CHECK-LABEL: epilogue:
 
+// CHECK-LABEL: define i32 @"kfun:#foo(kotlin.Any){}kotlin.Int
+fun foo(x: Any) = x as Int
+// CHECK-DEBUG: call zeroext i1 @IsSubtype
+// CHECK-OPT: call zeroext i1 @IsSubclassFast
+// CHECK-DEBUG-NOT: call zeroext i1 @IsSubtype
+// CHECK-OPT-NOT: call zeroext i1 @IsSubclassFast
+// CHECK-DEBUG: call i32 @"kfun:kotlin#<Int-unbox>(kotlin.Any){}kotlin.Int
+// CHECK-OPT: bitcast %struct.ObjHeader* %0 to %"kclassbody:kotlin.Int#internal"
+// CHECK-LABEL: epilogue:
+
 // CHECK-LABEL: define %struct.ObjHeader* @"kfun:#box(){}kotlin.String"
 fun box(): String {
     println(Foo(42))
+    println(foo(42))
     return "OK"
 }
