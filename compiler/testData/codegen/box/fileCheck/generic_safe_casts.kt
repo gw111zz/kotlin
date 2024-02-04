2bb511c0d4db07ecaa3e45ab8ab47020090f700b
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-02-04 09:17:42 +0200

    fixup! [K/N][tests] Added some file-check tests on generic safe casts

--- compiler/testData/codegen/box/fileCheck/generic_safe_casts.kt
+++ compiler/testData/codegen/box/fileCheck/generic_safe_casts.kt
@@ -3,8 +3,8 @@
 
 value class Foo(val value: Int)
 // CHECK-LABEL: define %struct.ObjHeader* @"kfun:Foo#$<bridge-NUN>toString(){}kotlin.String(){}kotlin.String
-// CHECK-DEBUG-NOT: call zeroext i1 @IsSubtype
-// CHECK-OPT-NOT: call zeroext i1 @IsSubclassFast
+// CHECK-DEBUG-NOT: {{call|call zeroext}} i1 @IsSubtype
+// CHECK-OPT-NOT: {{call|call zeroext}} i1 @IsSubclassFast
 // CHECK-LABEL: call %struct.ObjHeader* @"kfun:Foo#toString(){}kotlin.String
 // CHECK-LABEL: epilogue:
 
