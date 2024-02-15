1bbfb398a490f8830ccd8bb1067ac348f48a6bb0
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-02-15 12:49:36 +0100

    Align tests with the changed offsets for primary constructors

--- compiler/testData/diagnostics/testsWithJvmBackend/duplicateJvmSignature/inlineClasses/declarationClash.fir.kt
+++ compiler/testData/diagnostics/testsWithJvmBackend/duplicateJvmSignature/inlineClasses/declarationClash.fir.kt
@@ -1,10 +1,10 @@
 // WITH_STDLIB
 
 @JvmInline
-value class A<!CONFLICTING_JVM_DECLARATIONS!>(val x: Int)<!> {
+value class A<!CONFLICTING_JVM_DECLARATIONS!>(val x: Int) {
     <!CONFLICTING_JVM_DECLARATIONS!>constructor(x: UInt): this(x.toInt())<!>
-}
+}<!>
 
-data class B<!CONFLICTING_JVM_DECLARATIONS!>(val x: UInt)<!> {
+data class B<!CONFLICTING_JVM_DECLARATIONS!>(val x: UInt) {
     <!CONFLICTING_JVM_DECLARATIONS!>constructor(x: Int) : this(x.toUInt())<!>
-}
+}<!>
