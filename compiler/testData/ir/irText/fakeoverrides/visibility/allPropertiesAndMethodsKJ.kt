2771ecf1a198b0e1c4d38a6ada5cb189bd52fecf
Aleksandra.Arsenteva <aleksandra.arsenteva@jetbrains.com>
2024-02-15 16:18:30 +0300

    add tests static + getter/setter

--- compiler/testData/ir/irText/fakeoverrides/visibility/allPropertiesAndMethodsKJ.kt
+++ compiler/testData/ir/irText/fakeoverrides/visibility/allPropertiesAndMethodsKJ.kt
@@ -3,10 +3,10 @@
 // MODULE: separate
 // FILE: J2.java
 public class J2 {
-    public int j1;
-    protected int j2;
-    private int j3;
-    int j4;
+    public int j1 = 1;
+    protected int j2 = 2;
+    private int j3 = 3;
+    int j4 = 4;
 
     public void funJ1() {}
     protected void funJ2() {}
@@ -14,13 +14,21 @@ public class J2 {
     void funJ4() {}
 }
 
-// MODULE: main
+// FILE: Test.kt
+class I : J2() {
+    internal val j1 = 11
+    internal val j2 = 22
+    internal val j3 = 33
+    internal val j4 = 44
+}
+
+// MODULE: main(separate)
 // FILE: J.java
 public class J {
-    public int j1;
-    protected int j2;
-    private int j3;
-    int j4;
+    public int j1=12;
+    protected int j2=23;
+    private int j3=34;
+    int j4=45;
 
     public void funJ1() {}
     protected void funJ2() {}
@@ -45,7 +53,35 @@ class D : J2() {
     public override fun funJ2() { }
 }
 
-fun test(a: A, b: B, c: C, d: D) {
+class E: J() {
+    public val j1 = 100
+    public val j2 = 200
+    public val j3 = 300
+    public val j4 = 400
+}
+
+open class F : J() {
+    protected val j1 = 100
+    protected val j2 = 200
+    protected val j3 = 300
+    protected var j4 = 400
+}
+
+class G : J() {
+    internal val j1 = 100
+    internal val j2 = 200
+    internal val j3 = 300
+    internal var j4 = 400
+}
+
+class H : J() {
+    private val j1 = 100
+    private val j2 = 200
+    private val j3 = 300
+    private val j4 = 400
+}
+
+fun test(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I) {
     a.j1 = 1
     a.j2 = 2
     a.j4 = 3
@@ -68,4 +104,26 @@ fun test(a: A, b: B, c: C, d: D) {
     d.j1 = 1
     d.funJ1()
     d.funJ2()
+
+    e.j1
+    e.j2
+    e.j3
+    e.j4
+
+    f.j1
+    f.j2
+    f.j4
+
+    g.j1
+    g.j2
+    g.j3
+    g.j4
+
+    h.j1
+    h.j2
+    h.j4
+
+    i.j1
+    i.j2
+    i.j4
 }
