1dae7cebadc692807c577a324e471388040e4a35
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-02-16 21:03:22 +0100

    [AA Standalone] Support finding compiled elements by PSI/FIR comparison
    - This commit revives `KtDeclarationAndFirDeclarationEqualityChecker`,
      which was removed in the following commit: 92b6a77. The functionality
      was removed under the assumption that deserialized elements now carry
      their PSI because they are stub-based. This is not true for the
      Standalone API, which does not use stub-based deserialization.
    - In contrast to `KtDeclarationAndFirDeclarationEqualityChecker`, the
      new `LLFirElementByPsiElementChooser` allows us to provide a
      considerably simpler implementation for the IDE mode, where we can
      rely on `FirElement.realPsi`. This removes the risk that the "hacky"
      Standalone implementation affects the IDE.
    - `KtDeclarationAndFirDeclarationEqualityChecker`'s old implementation
      is reused in `LLStandaloneFirElementByPsiElementChooser`.
    
    ^KT-65813 fixed

--- generators/analysis-api-generator/tests/org/jetbrains/kotlin/generators/tests/analysis/api/analysisApi.kt
+++ generators/analysis-api-generator/tests/org/jetbrains/kotlin/generators/tests/analysis/api/analysisApi.kt
@@ -583,8 +583,7 @@ private fun AnalysisApiTestGroup.generateAnalysisApiComponentsTests() {
         }
     }
 
-    // Avoid Standalone mode because of KT-65813.
-    component("visibilityChecker", filter = frontendIs(FrontendKind.Fir) and analysisApiModeIs(AnalysisApiMode.Ide)) {
+    component("visibilityChecker", filter = frontendIs(FrontendKind.Fir)) {
         test(AbstractVisibilityCheckerTest::class) {
             model(it, "visibility")
         }
