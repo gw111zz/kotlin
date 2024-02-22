46b867219a3ec992ce8e287d5276541f419a9af2
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-02-22 10:04:59 +0200

    [Test] Add regression test for KT-65493

---
+++ compiler/testData/ir/irText/fakeOverrides/intersectionWithRawType.kt
@@ -0,0 +1,17 @@
+// ENABLE_IR_FAKE_OVERRIDE_GENERATION
+// TARGET_BACKEND: JVM_IR
+// ISSUE: KT-65493
+
+// FILE: JavaBase.java
+import java.util.*;
+
+public class JavaBase {
+    public List bar() { return null; };
+}
+
+// FILE: main.kt
+interface KotlinInterface {
+    fun bar(): List<Any?>
+}
+
+abstract class Derived: JavaBase(), KotlinInterface
